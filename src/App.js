35947582ddd7e9ee0f1cb6cf84cc8105b61a81ad
Taras-js <78747117+Taras-js@users.noreply.github.com>
2022-11-05 23:11:48 +0300

    fix: https://e061-31-135-77-185.ngrok.io

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     const [products, setProducts] = useState(null)
     useEffect(() => {
         tg.ready();
-        fetch('https://taras-server.ru/code', {
+        fetch('/code', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
